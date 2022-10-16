5f0ca7fcb4a485613a56c02c1f4aa43d621aed1e
Dev-Raaz <singhdebraj51@gmail.com>
2022-10-16 10:43:04 +0530

    Fixed table layout issues

--- src/components/StocksTable.js
+++ src/components/StocksTable.js
@@ -4,45 +4,47 @@ import { Link } from 'react-router-dom'
 const StocksTable = ({ tableData }) => {
 
     return (
-        <table className='stocks-table'>
-            {/* Head starts here */}
-            <thead sx={{width: '100%'}}>
-                <tr>
-                    <th>
-                        <p>Symbol</p>
-                    </th>
-                    <th>
-                        <p>Name</p>
-                    </th>
-                    <th>
-                        <p>Category</p>
-                    </th>
-                </tr>
-            </thead>
+        <div className='table-wrapper'>
+            <table className='stocks-table'>
+                {/* Head starts here */}
+                <thead sx={{width: '100%'}}>
+                    <tr>
+                        <th>
+                            <p>Symbol</p>
+                        </th>
+                        <th>
+                            <p>Name</p>
+                        </th>
+                        <th>
+                            <p>Category</p>
+                        </th>
+                    </tr>
+                </thead>
 
-            {/* Body starts here */}
-            <tbody>
-                {
-                    tableData.map(row => (
-                        <tr key={row.Symbol}>
-                            <td>
-                                <Link to={`/${row.Symbol}`} state={row}>
-                                    {row.Symbol}
-                                </Link>
-                            </td>
+                {/* Body starts here */}
+                <tbody>
+                    {
+                        tableData.map(row => (
+                            <tr key={row.Symbol}>
+                                <td>
+                                    <Link to={`/${row.Symbol}`} state={row}>
+                                        {row.Symbol}
+                                    </Link>
+                                </td>
 
-                            <td>
-                                <p>{row.Name}</p>
-                            </td>
+                                <td>
+                                    <p>{row.Name}</p>
+                                </td>
 
-                            <td>
-                                <p>{row.Sector || "None"}</p>
-                            </td>
-                        </tr>
-                    ))
-                }
-            </tbody>
-        </table>
+                                <td>
+                                    <p>{row.Sector || "None"}</p>
+                                </td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </div>
     )
 }
 export default StocksTable
